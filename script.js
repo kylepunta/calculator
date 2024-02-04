c04b3d0bd140b5e1a6634fdd659da25c31465496
kylepunta <kylepurcell1998@gmail.com>
2024-02-04 11:47:35 +0000

    Added a script to change the background colours of the button elements on mouse click and commented it out

---
+++ script.js
@@ -0,0 +1,9 @@
+// let buttons = document.querySelectorAll('button');
+// buttons.forEach((button) => {
+//     button.addEventListener('mousedown', () => {
+//         button.classList.add('mouse-down');
+//     });
+//     button.addEventListener('mouseup', () => {
+//         button.classList.remove('mouse-down');
+//     });
+// });
