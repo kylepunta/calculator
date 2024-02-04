92fb3f7e2a58a4af50c36f7489ee204292abf52a
kylepunta <kylepurcell1998@gmail.com>
2024-02-04 14:47:27 +0000

    Added return statements in functions with arithmetic operations

--- script.js
+++ script.js
@@ -3,17 +3,17 @@ let lastNumber = 0;
 let operator = "";
 
 function add(firstNumber, lastNumber){
-
+    return firstNumber + lastNumber;
 };
 
 function subtract(firstNumber, lastNumber){
-
+    return firstNumber - lastNumber;
 };
 
 function multiply(firstNumber, lastNumber){
-
+    return firstNumber * lastNumber;
 };
 
 function divide(firstNumber, lastNumber){
-
+    return firstNumber / lastNumber;
 };
