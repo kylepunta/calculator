5bcd49160eec286fd649f50af4982fc41a00c2ef
kylepunta <kylepurcell1998@gmail.com>
2024-02-04 17:16:47 +0000

    Changed switch case values; added event listener click functions to operands and operators

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
+let currentNumber = 0;
 let firstNumber = 0;
 let lastNumber = 0;
-let operator = '';
+let currentOperator = 'empty';
+let result = 0;
 
 function add(firstNumber, lastNumber){
     return firstNumber + lastNumber;
@@ -22,16 +24,16 @@ function operate(firstNumber, lastNumber, operator){
     let result = 0;
 
     switch (operator) {
-        case '+':
+        case 'add':
                     result = add(firstNumber, lastNumber);
                     break;
-        case '-':
+        case 'subtract':
                     result = subtract(firstNumber, lastNumber);
                     break;
-        case '*':
+        case 'multiply':
                     result = multiply(firstNumber, lastNumber);
                     break;
-        case '/':
+        case 'divide':
                     result = divide(firstNumber, lastNumber);
                     break;
     };
@@ -41,7 +43,86 @@ function operate(firstNumber, lastNumber, operator){
 let display = document.querySelector('.value');
 let operands = document.querySelectorAll('.operand');
 let operators = document.querySelectorAll('.operator');
-let reset = document.querySelectorAll('.reset');
-let clearEntry = document.querySelectorAll('.remove');
-let decimal = document.querySelectorAll('.decimal');
-let modulo = document.querySelectorAll('.modulo');
+let operateButton = document.querySelector('.operate');
+let reset = document.querySelector('.reset');
+let clearEntry = document.querySelector('.remove');
+let decimal = document.querySelector('.decimal');
+let modulo = document.querySelector('.modulo');
+
+display.textContent = 0;
+
+console.log(currentNumber);
+console.log(firstNumber);
+console.log(lastNumber);
+console.log(currentOperator);
+
+
+operands.forEach((operand) => {
+    operand.addEventListener('click', () => {
+        if (firstNumber == 0) {
+            if (display.textContent == 0) {
+                currentNumber = operand.textContent;
+                display.textContent = currentNumber;
+            }
+            else {
+                currentNumber += operand.textContent;
+                display.textContent = currentNumber;    
+            };
+            console.log(currentNumber);
+            console.log(firstNumber);
+            console.log(lastNumber);
+            console.log(currentOperator);
+
+        }
+        else {
+            display.textContent += operand.textContent;
+            console.log(currentNumber);
+            console.log(firstNumber);
+            console.log(lastNumber);
+            console.log(currentOperator);
+
+        };
+    });
+});
+
+reset.addEventListener('click', () => {
+    display.textContent = 0;
+    currentNumber = 0;
+    firstNumber = 0;
+    lastNumber = 0;
+    currentOperator = '';
+});
+
+operators.forEach((operator) => {
+    operator.addEventListener('click', () => {
+        if (firstNumber == 0) {
+            firstNumber = currentNumber;
+            currentNumber = 0;
+            display.textContent += " " + operator.textContent + " ";
+            currentOperator = operator.id;
+            console.log(currentNumber);
+            console.log(firstNumber);
+            console.log(lastNumber);
+            console.log(currentOperator);
+
+        }
+        else {
+            lastNumber = currentNumber;
+            result = operate(firstNumber, lastNumber, currentOperator);
+            display.textContent = result;
+            firstNumber = result;
+            currentOperator = operator.id;
+            console.log(currentNumber);
+            console.log(firstNumber);
+            console.log(lastNumber);
+            console.log(currentOperator);
+
+        };
+    });
+});
+
+operateButton.addEventListener('click', () => {
+    lastNumber = currentNumber;
+    result = operate(firstNumber, lastNumber, currentOperator);
+    display.textContent = result;
+});
