ec0504b3c4343cac22c14729ae016dca2a7ca5f9
kylepunta <kylepurcell1998@gmail.com>
2024-02-05 13:05:40 +0000

    Removed all calculator functionality in the operand and operator event listeners

--- script.js
+++ script.js
@@ -78,27 +78,7 @@ console.log("Result " + result);
 
 operands.forEach((operand) => {
     operand.addEventListener('click', () => {
-        if (result == 0) {
-            if (currentNumber == 0) {
-                currentNumber = operand.textContent;
-                display.textContent = currentNumber;
-            }
-            else {
-                currentNumber += operand.textContent;
-                display.textContent = currentNumber;
-            };    
-        }
-        else {
-            if (firstNumber == 0 || currentNumber == 0) {
-                firstNumber = result;
-                currentNumber = operand.textContent;
-                display.textContent = currentNumber;
-            }
-            else {
-                currentNumber += operand.textContent;
-                display.textContent = currentNumber;
-            };
-        };
+        
     });
 });
 
@@ -113,56 +93,22 @@ reset.addEventListener('click', () => {
 
 operators.forEach((operator) => {
     operator.addEventListener('click', () => {
-        if (firstNumber == 0) {
-            firstNumber = currentNumber;
-            currentNumber = 0;
-            currentOperator = operator.id;
-        }
-        else {
-            lastNumber = currentNumber;
-            result = operate(firstNumber, lastNumber, currentOperator);
-            display.textContent = result;
-            currentOperator = operator.id;
-            firstNumber = 0;
-            lastNumber = 0;
-            currentNumber = result; 
-        };
+        
     });
 });
 
 operateButton.addEventListener('click', () => {
-    lastNumber = currentNumber;
-    result = operate(firstNumber, lastNumber, currentOperator);
-    if (currentOperator == "divide" && lastNumber == 0) {
-        display.textContent = "ERROR";
-        currentNumber = 0;
-        firstNumber = 0;
-        lastNumber = 0;
-        currentOperator = "none";   
-    }
-    else {
-        display.textContent = result;
-        currentNumber = result;
-        firstNumber = 0;
-        lastNumber = 0;
-        currentOperator = "none";   
-    }
+   
 });
 
 clearEntry.addEventListener('click', () => {
-    if (currentNumber != "") {
+    if (display.textContent != "") {
         display.textContent = display.textContent.slice(0, display.textContent.length - 1);
-        currentNumber = currentNumber.slice(0, currentNumber.length - 1);
-    }
-    else {
-        if (currentOperator != "none") {
-            currentOperator = "none";
+        currentNumber = display.textContent;
+        if (currentNumber == "") {
             currentNumber = 0;
-            display.textContent = display.textContent.slice(0, display.textContent.length - 3);
-        }
-        else {
-            currentNumber = 0;
-        };
+            display.textContent = currentNumber;
+        };    
     };
 });
 
